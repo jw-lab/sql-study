25b76c5ca50ca59a1082bd45f9d91e309ef68041
jw <jdb0731@naver.com>
2021-10-28 10:09:40 +0900

    1028 오전

--- 11_인덱스와시퀀스.sql
+++ 11_인덱스와시퀀스.sql
@@ -49,3 +49,53 @@ FROM members
 WHERE last_name LIKE 'A%' AND first_name LIKE 'M%';
 --저장된 실행보고서 읽기
 SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
+
+
+--시퀀스 : 유일(중복이 안 되는)값을 생성/자동으로 순차적 증가/보통 PRIMARY KEY(ID) 값을 생성하기 위해 사용/테이블과는 독립적으로 저장되고 생성된다.
+CREATE SEQUENCE 시퀀스1; --생성
+DROP SEQUENCE 시퀀스1; --삭제
+
+SELECT * FROM USER_SEQUENCES WHERE sequence_name = '시퀀스1';
+
+--시퀀스 테스트 (이름.NEXTVAL => 실행하면 값이 증가)
+SELECT 시퀀스1.NEXTVAL FROM DUAL;
+
+
+CREATE TABLE 부서_테스트 (
+    부서번호    NUMBER PRIMARY KEY,
+    부서이름    VARCHAR2(100)
+);
+
+INSERT INTO 부서_테스트(부서번호,부서이름) VALUES(시퀀스1.NEXTVAL,'영업부');
+INSERT INTO 부서_테스트(부서번호,부서이름) VALUES(시퀀스1.NEXTVAL,'개발부');
+
+SELECT * FROM 부서_테스트;
+
+--시퀀스의 값은 증가시키지 않고 현재 값을 알고 싶을때 (이름.CURRVAL)
+SELECT 시퀀스1.CURRVAL FROM DUAL;
+
+--시퀀스 옵션 시작값 10 부터 증가는 20
+DROP SEQUENCE 시퀀스2;
+CREATE SEQUENCE 시퀀스2
+START WITH 10
+INCREMENT BY 20;
+--시퀀스 확인
+SELECT * FROM USER_SEQUENCES WHERE sequence_name = '시퀀스2';
+
+--입력
+INSERT INTO 부서_테스트(부서번호,부서이름) VALUES(시퀀스2.NEXTVAL,'마케팅부');
+INSERT INTO 부서_테스트(부서번호,부서이름) VALUES(시퀀스2.NEXTVAL,'교육부');
+INSERT INTO 부서_테스트(부서번호,부서이름) VALUES(시퀀스2.NEXTVAL,'경영부');
+SELECT * FROM 부서_테스트 ORDER BY 부서번호;
+
+
+--시퀀스로 전체 번호 업데이트
+UPDATE 부서_테스트
+SET 부서번호=시퀀스2.NEXTVAL;
+
+--시퀀스 수정 :START WITH를 제외하고 수정가능
+--시퀀스2의 증가값을 2로 수정
+ALTER SEQUENCE 시퀀스2
+INCREMENT BY 2;
+
+DROP SEQUENCE 시퀀스2;
